53eb4a5c82ebc63911e61a6780812f0410152e3b
tei219 <tei219@gmail.com>
2024-05-27 10:40:04 +0000

    Mon May 27 10:40:04 UTC 2024

--- 213/docker-entrypoint-initdb.d/01_create-user.sql
+++ 213/docker-entrypoint-initdb.d/01_create-user.sql
@@ -1,4 +1,5 @@
-create user repl@'%'identified by 'repl';
+set session sql_log_bin = 0;
+create user repl@'%' identified by 'repl';
 grant replication slave on *.* to repl@'%';
 grant backup_admin on *.* to repl@'%';
 flush privileges;
